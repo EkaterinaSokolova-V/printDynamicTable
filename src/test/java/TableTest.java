46f99f9348b62f950c4806a30b62b3f6fe684e6b
Elisey93 <eliseyrodriguez@gmail.com>
2023-10-05 20:28:04 +0100

    fix: demoqa WebTables now prints

--- src/test/java/TableTest.java
+++ src/test/java/TableTest.java
@@ -4,32 +4,35 @@ import org.testng.annotations.Test;
 
 import java.util.List;
 
+import static org.testng.Assert.assertFalse;
+
 public class TableTest extends BaseTest {
 
-//    @Test
-//    public void printGoogleTable() {
-//        WebPage googlePage = new WebPage(getDriver());
+    @Test
+    public void printGoogleTable() {
+
+        WebPage googlePage = new WebPage(getDriver());
 //        List<List<String>> table = googlePage.getRows();
-////        List<String> table = googlePage.getTable();
-//        System.out.println(table);
-//
-//        for (int i = 0; i < table.size(); i++) {
-//            int ind = table.get(i).lastIndexOf("\n");
-//
-////            String[] row = table.get(i).substring(0, ind).split("\n");
-//
-//            for (int j = 0; j < row.length; j++) {
-//                String s = String.format("%-32s", row[j]);
-//                System.out.print(s);
-//            }
-//            System.out.println();
-//        }
-//    }
+        List<String> table = googlePage.getTable();
+        System.out.println(table);
+
+        for (int i = 0; i < table.size(); i++) {
+            int ind = table.get(i).lastIndexOf("\n");
+
+            String[] row = table.get(i).substring(0, ind).split("\n");
+
+            for (int j = 0; j < row.length; j++) {
+                String s = String.format("%-32s", row[j]);
+                System.out.print(s);
+            }
+            System.out.println();
+        }
+    }
 
     @Test
     public void printDemoWebTables() {
-        var webTable = new WebPage(getDriver());
-        var table = webTable.getRows();
+        WebPage webTable = new WebPage(getDriver());
+        List<List<String>> table = webTable.getRows();
 
         for (List<String> row : table) {
             StringBuilder builder = new StringBuilder();
@@ -39,5 +42,7 @@ public class TableTest extends BaseTest {
             System.out.println(builder);
             System.out.println();
         }
+
+        assertFalse(table.isEmpty());
     }
 }
--- src/test/java/model/WebPage.java
+++ src/test/java/model/WebPage.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,14 +18,14 @@ public class WebPage extends BasePage {
         super(driver);
     }
 
-//    public List<String> getTable() {
-//        return listAllRows.stream().map(WebElement::getText)
-//                .collect(Collectors.toList());
-//    }
+    public List<String> getTable() {
+        return listAllRows.stream().map(WebElement::getText)
+                .collect(Collectors.toList());
+    }
 
     public List<List<String>> getRows() {
         return listAllRows.stream().map(row -> row.findElements(By.className("rt-td")))
-                .map(list -> list.stream().map(WebElement::getText)
+                .map(listElement -> listElement.stream().map(WebElement::getText)
                         .collect(Collectors.toList()))
                 .collect(Collectors.toList());
     }
